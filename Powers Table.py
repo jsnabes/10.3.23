7e58cb4b874e92b454f3f3f5dd50bc6d1918a9b5
Julia Snabes <julia.snabes@gmail.com>
2023-10-03 15:00:15 -0500

    afternoon

---
+++ Powers Table.py
@@ -0,0 +1,32 @@
+# Get integer
+print("Learn your squares and cubes!")
+keep_going = "y"
+# Continue asking user to enter an integer
+while keep_going.lower() == "y":
+    prompt = int(input("Enter an integer: "))
+# Print table results
+    print("Number   Squared     Cubed")
+    print("======   =======     =====")
+    for i in range(prompt):
+        i += 1
+        print(f"{i}         {i ** 2}          {i ** 3}")
+
+# # Extended challenge
+#     print("Here's a multiplication table for reference!")
+# # Print horizontal lines of multi table
+#     horizontal = ""
+#     for i in range(prompt):
+#         i += 1
+#         horizontal += str(i) + "    "
+#         print(horizontal)
+#  # Print vertical lines of multi table
+#     for i in range(prompt):
+#         i += 1
+#     print(f"{i}   |    {i * 1}    {i * 3}     {i * 4}")
+
+# Ask to continue. If n program will end
+    keep_going = input("Continue? (y/n): ")
+
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+# Print question and ask user to enter integer
+prompt = int(input("Enter an integer between 1 and 100: "))
+list = range(0,101)
 
+# First if statement option
+for num in list:
+    if prompt < 60 and num % 2 != 0 :
+        print(f"You entered {prompt}")
+        print("Odd and less than 60")
+
+# Second if statement option
+    if prompt <24 and prompt <1 and num
+        print(f"You entered {prompt}")
+        print("Even and less than 25")
+
+# Third if statement option
 
